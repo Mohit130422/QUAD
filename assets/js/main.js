85d6bfb49691d2e58b1de14f42f7074262aa483d
Mohit Varshney <mohit.varshney@perceptknorigin.com>
2023-10-27 18:20:59 +0530

    quad first commit- with hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,25 @@
-//navbar toggle on mobile view
-let menuIcon = document.querySelector('#btn-menu');
-let navbar =  document.querySelector('.nav-links');
-
-menuIcon.onclick = () =>{
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('nav-on');
-}
-
-//scroll header section
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () =>{
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
-    let height = sec.offsetHeight;
-    let id  = sec.getAttribute('id');
-
-    if (top >= offset && top < offset + height){
-      navLinks.forEach(links => {
-        links.classList.remove('active-link');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active-link');
-      });
+$(document).ready(function () {
+  $("#hero-banner").owlCarousel({
+    loop: true,
+    margin: 16,
+    dots: true,
+    nav: true,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
     }
-
-    let header = document.querySelector('header');
-    header.classList.toggle('scroll-head', window.scrollY > 100)
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('nav-on');
-
   });
-}
+});
+
 
